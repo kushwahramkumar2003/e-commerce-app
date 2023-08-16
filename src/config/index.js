4a5438aefc369956ac4e3006a164032985c50f8f
Ramkumar kushwah <kushwahramkumar2003@gmail.com>
2023-08-16 11:57:28 +0530

    product schema

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,10 @@
 import dotenv from "dotenv";
 dotenv.config();
 const config = {
-    PORT : process.env.PORT || 5000,
-    MONGODB_URL:process.env.MONGODB_URL || "mongodb://localhost:27127/e-comm",
-}
+  PORT: process.env.PORT || 5000,
+  MONGODB_URL: process.env.MONGODB_URL || "mongodb://localhost:27127/e-comm",
+  JWT_SECRET: process.env.JWT_SECRET || "yoursecret",
+  JWT_EXPIRY: process.env.JWT_EXPIRY || "30d",
+};
 export default config;
+bcrypt
