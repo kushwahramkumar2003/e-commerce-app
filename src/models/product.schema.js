0daea82f90395c001a7aecc3ef72f6147fbd938d
Ramkumar kushwah <kushwahramkumar2003@gmail.com>
2023-08-24 08:15:30 +0530

    higher order func for controller

--- src/models/product.schema.js
+++ src/models/product.schema.js
@@ -32,8 +32,8 @@ const productSchema = new mongoose.Schema(
       default: 0,
     },
     collectionId: {
+      type: mongoose.Schema.Types.ObjectId,
       ref: "Collection",
-      
     },
   },
   { timestamps: true }
---
+++ src/service/asyncHandler.js
@@ -0,0 +1,18 @@
+const asyncHandler = (fn) => async (req, res, next) => {
+  try {
+    await fn(req, res, next);
+  } catch (error) {
+    res.status(error.code || 500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+export default asyncHandler;
+
+// const asyncHandler = "Ramkumar";
+// const asyncHandler = ()=>{};
+// const asyncHandler = (func)=>{};
+// const asyncHandler = (func)=>()=>{};
+// const asyncHandler = (func) => async () => {};
